ec7da82bcf9932e2d40bd4435a4630d7c6c9288a
TheGazdaman <michal.gazdik@livesport.eu>
2021-04-16 12:07:55 +0200

    serialize, deserialize with try and catch error

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,15 +3,29 @@
 
 import * as React from 'react'
 
-function useLocalStorageWithState(key, defaultValue = '', {serialize = JSON.stringify, deserialize = JSON.parse} = {}) {
+function useLocalStorageWithState(key, defaultValue = '', {serialize = JSON.stringify, deserialize = JSON.parse} = {},) {
     const [state, setState] = React.useState(() => {
-      const valueInLocaleStorage = window.localStorage.getItem(key);
-      if (valueInLocaleStorage) {
-        return deserialize(valueInLocaleStorage)
+      const valueInLocalStorage = window.localStorage.getItem(key);
+      if (valueInLocalStorage) {
+          try {
+              return deserialize(valueInLocalStorage)
+          }
+          catch (error) {
+              window.localStorage.removeItem(key)
+          }
       }
-      return defaultValue;
+      return typeof defaultValue === 'function' ? defaultValue() : defaultValue;
     });
+
+    const prevKeyRef = React.useRef(key);
+
       React.useEffect(() => {
+          const prevKey = prevKeyRef.current;
+          if(prevKey !== key) {
+              window.localStorage.removeItem(prevKey)
+          }
+          prevKeyRef.current = key;
+
         window.localStorage.setItem(key, serialize(state))
       }, [key, serialize, state]);
 
